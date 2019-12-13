0ed6c4f539b346d681b1aaa85cdcd30c8ea71b47
Gerz2011 <30986978+gerz2011@users.noreply.github.com>
2019-12-13 20:28:04 +0300

    12

--- django_app_menu/app_site/tests.py
+++ django_app_menu/app_site/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.db import connection
 
-# Create your tests here.
+
+class TestSomething(TestCase):
+
+    def test_something(self):
+        counter = len(connection.queries)
+        something()
+        self.assertEquals(len(connection.queries), counter)
