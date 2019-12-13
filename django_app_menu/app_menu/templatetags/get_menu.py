7814ad8424e298466f89b7f2dccc508adfe44577
Gerz2011 <30986978+gerz2011@users.noreply.github.com>
2019-12-13 04:43:19 +0300

    11
    ***

---
+++ django_app_menu/app_menu/templatetags/get_menu.py
@@ -0,0 +1,16 @@
+from django import template
+
+from app_menu.models import Menu
+
+register = template.Library()
+
+
+
+@register.inclusion_tag('include/menu.html', takes_context=True)
+def draw_menu(context, slug):
+    try:
+        menu = Menu.objects.prefetch_related('items').get(slug=slug)
+        return {'menu': menu, 'context': context}
+    except Menu.DoesNotExist:
+        return {'menu': '', 'context': context}
+
---
+++ django_app_menu/app_site/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class home_detail(TemplateView):
+    template_name = 'home-page.html'
+
+
+class page_detail(TemplateView):
+    template_name = 'site-page.html'
+
