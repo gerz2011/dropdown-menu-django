59b318465ceb74361799cc82f72ad8e37029797f
Gerz2011 <30986978+gerz2011@users.noreply.github.com>
2019-12-16 03:48:59 +0300

    решено

--- django_app_menu/app_menu/templatetags/get_menu.py
+++ django_app_menu/app_menu/templatetags/get_menu.py
@@ -1,36 +1,32 @@
 from django import template
 from django.db import connection
-from app_menu.models import Menu
+from app_menu.models import Menu, MenuItem
+from django.db import reset_queries
+
 
 register = template.Library()
-
-
+  
 
 @register.inclusion_tag('include/menu.html', takes_context=True)
 def draw_menu(context, slug):
     try:
-        menu = Menu.objects.get(slug='main-menu')
-        print(connection.queries)
+        # menu = Menu.objects.raw('SELECT * FROM app_menu_menuitem WHERE app_menu_menuitem.menu_id IN (1)')
+        # menu = MenuItem.objects.raw('SELECT * FROM app_menu_menuitem')
+        # menu = Menu.objects.prefetch_related('items').get(slug="main-menu")
+        menu = MenuItem.objects.filter(menu__slug=slug)
+        # menu = Menu.objects.get(slug=slug).filter()      
+
+        # print(menu)
+
+
+        print('======================================')
+        print('SQL запрос draw_menu - ', connection.queries)
+        print('запросов - ', len(connection.queries))
+        reset_queries()
+        print('--------------------------------------')
+
         return {'menu': menu, 'context': context}
     except Menu.DoesNotExist:
         return {'menu': '', 'context': context}
 
 
-"""
-menu = Menu.objects.get(slug='main-menu')
-[
-{'sql': 'SELECT "app_menu_menu"."id", "app_menu_menu"."is_visible", "app_menu_menu"."order", "app_menu_menu"."date_created", "app_menu_menu"."date_updated", "app_menu_menu"."title", "app_menu_menu"."slug", "app_menu_menu"."named_url" FROM "app_menu_menu" WHERE "app_menu_menu"."slug" = \'main-menu\' LIMIT 21', 'time': '0.000'}
-]
-
------------------------------
-menu = Menu.objects.prefetch_related('items').get(slug="main-menu")
-[
-{
-'sql': 'SELECT "app_menu_menu"."id", "app_menu_menu"."is_visible", "app_menu_menu"."order", "app_menu_menu"."date_created", "app_menu_menu"."date_updated", "app_menu_menu"."title", "app_menu_menu"."slug", "app_menu_menu"."named_url" FROM "app_menu_menu" WHERE "app_menu_menu"."slug" = \'main-menu\' LIMIT 21', 
-'time': '0.000'}, 
-{
-'sql': 'SELECT "app_menu_menuitem"."id", "app_menu_menuitem"."is_visible", "app_menu_menuitem"."order", "app_menu_menuitem"."date_created", "app_menu_menuitem"."date_updated", "app_menu_menuitem"."menu_id", "app_menu_menuitem"."parent_id", "app_menu_menuitem"."title", "app_menu_menuitem"."url", "app_menu_menuitem"."named_url" FROM "app_menu_menuitem" WHERE "app_menu_menuitem"."menu_id" IN (1) ORDER BY "app_menu_menuitem"."order" ASC',
-'time': '0.000'}
-]
-
-"""
--- django_app_menu/app_site/views.py
+++ django_app_menu/app_site/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class home_detail(TemplateView):
     template_name = 'home-page.html'
 
-
 class page_detail(TemplateView):
     template_name = 'site-page.html'
-
